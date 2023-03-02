d3e6de806154af7d75c4ef91d3fa9d1c64282e87
leslymathew-1 <cerlinmathew2@gmail.com>
2023-03-02 16:09:41 +0530

    here

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,61 +1,74 @@
 export const district=[
     {
         name: "Alappuzha",
-        Image:"img.jpeg"
+        Image:"img.jpeg",
+        desc:"Alappuzha (or Alleppey) is a city on the Laccadive Sea in the southern Indian state of Kerala. It's best known for houseboat cruises along the rustic Kerala backwaters, a network of tranquil canals and lagoons."
+    
     
     },
     {
         name: "Ernakulam",
-        Image:"ernakulam.jpeg"
+        Image:"ernakulam.jpeg",
+        desc: "Sprawling, residential Ernakulam is known for Marine Drive, a busy waterfront promenade where boats offer backwater cruises. The Kerala Folklore and Hill Palace museums explore local heritage through art and antiquities. "
     },
     
     {
-        name: "Kozhikod",
-        Image:"kozhikod.jpeg"
+        name: "Kozhikode",
+        Image:"kozhikod.jpeg",
+        desc:"Kozhikode is a coastal city in the south Indian state of Kerala. It was a significant spice trade center and is close to Kappad Beach, where Portuguese explorer Vasco da Gama landed in 1498."
     },
     {
         name: "Palakkad",
-        Image:"palakkad.jpeg"
+        Image:"palakkad.jpeg",
+        desc: "Palakkad is one of the fourteen districts of Kerala and has no coastal line. The district is one of the main granaries of Kerala and its economy is primarily agricultural. The district is also the land of Palmyrahs."
     },
     {
         name: "Kollam",
-        Image:"kollam.jpeg"
+        Image:"kollam.jpeg",
+        desc: "Kollam is a city in the state of Kerala, on India's Malabar Coast. It’s known as a trade hub and for its beaches, like lively Kollam and secluded Thirumullavaram. "
     },
     {
         name: "Kannur",
-        Image:"kannur.jpeg"
+        Image:"kannur.jpeg",
+        desc: "Kannur is a coastal city in the south Indian state of Kerala. It was once an ancient trading port. Enduring monuments such as 16th-century St. Angelo Fort, once occupied by European colonial forces, show the city’s significant role in the spice trade. "
     },
     {
         name: "Kasaragod",
-        Image:"kasargod.jpeg"
+        Image:"kasargod.jpeg",
+        desc:"Kasaragod is a municipal town and administrative headquarters of Kasaragod district in the state of Kerala, India. It was the first municipal town in the district. It is the northernmost district of Kerala and is also known as Saptha Bhasha Sangama Bhoomi"
     },
     {
         name: "Idukki",
-        Image:"idukki.jpeg"
+        Image:"idukki.jpeg",
+        desc: "Idukki district is a densely forested, mountainous region in the south Indian state of Kerala. In the north, Anamudi mountain towers over Eravikulam National Park, where the rare, blue Neelakurinji flower blooms every 12 years."
     },
 
     {
         name: "Kottayam",
-        Image:"kottayam.jpeg"
+        Image:"kottayam.jpeg",
+        desc:"Kottayam is a municipal town in the Indian state of Kerala. Flanked by the Western Ghats on the east and the Vembanad Lake and paddy fields of Kuttanad on the west."
     },
     {
         name: "Pathanamthitta",
-        Image:"pathanamthitta.jpeg"
+        Image:"pathanamthitta.jpeg",
+        desc:"Situated near the Western Ghats and bordered by the hills, Pathanamthitta district is a treat to eyes with its vast unending stretches of forests, rivers and rural landscapes. Blessed by nature, the district is famous for its scenic beauty, fairs and festivals."
     },
     {
         name: "Thrissur",
-        Image:"thrissur.jpeg"
+        Image:"thrissur.jpeg",
+        desc:"Thrissur is known as the ‘cultural capital of Kerala’ for more reasons than one. Its annual calendar is filled with festivals and events, both cultural and religious, which attract thousands of visitors to the town every year."
     },
     {
         name: "Malappuram",
-        Image:"malappuram.jpeg"
-    },
-    {
+        Image:"malappuram.jpeg",
+        desc: "Bounded by the Nilgiris hills on the East and the Arabian Sea on the west, the district of Malappuram literally the land atop the hills, is remarkable for its unique natural beauty."},{
         name: "Wayanad",
-        Image:"wayanad.jpeg"
+        Image:"wayanad.jpeg",
+        desc:"Malappuram is a city in the Indian state of Kerala, spread over an area of 58.20 km² including the surrounding suburban areas."
     },
     {
         name: "Thiruvananthapuram",
-        Image:"tvm.jpeg"
+        Image:"tvm.jpeg",
+        desc:"The southern tip of God’s Own Country is home to the administrative and cultural hub of the state. Young and old alike, all can experience their own slice of Kerala here, tailor-made to their individual preference."
     }
 ]
